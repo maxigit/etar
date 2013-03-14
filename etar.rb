b1ade771ae131d00fa9bc70b6ac6e4e915ac3291
max <bmx007@gmail.com>
2013-03-14 16:46:30 +0000

    Don't follow symlink and create directory for each file

--- etar.rb
+++ etar.rb
@@ -6,17 +6,20 @@ IGNORES << /^\./  # ignore hidden files
 def etar(paths) 
   paths = Array(paths)
   paths.each do |path|
-    puts "mkdir -p #{path}"
     Dir.entries(path).each do |entry|
       fullname = File.join(path, entry)
 
       next if IGNORES.inject(false) { |v, ig| v || entry =~ ig }
       case
+      when File.symlink?(fullname)
+        next
       when File.directory?(fullname)
         etar(fullname)
       when File.file?(fullname)
         key = "__END_OF_#{entry}__"
-        puts "cat <<#{key} > #{fullname}"
+        puts "filename='#{fullname}'"
+        puts "mkdir -p \$(dirname $filename)"
+        puts "cat <<#{key} > $filename"
         File.open(fullname).each_line do |line|
           puts line.gsub(/([$\\`])/, '\\\\\1')
         end
